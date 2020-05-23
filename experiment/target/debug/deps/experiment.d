0580a9e88035bfb7d9ac99aca28c5aee0fdb9d9e
Shan Newton <shan.alexander2020@gmail.com>
2020-05-24 00:30:06 +0700

    removes experiment folder

--- experiment/target/debug/deps/experiment.d
+++
@@ -1,6 +0,0 @@
-E:\Projects\Rust\console\finance-strongsauce\experiment\target\debug\deps\experiment.exe: src\main.rs src\fina.rs
-
-E:\Projects\Rust\console\finance-strongsauce\experiment\target\debug\deps\experiment.d: src\main.rs src\fina.rs
-
-src\main.rs:
-src\fina.rs:
